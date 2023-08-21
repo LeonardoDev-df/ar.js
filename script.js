b745db98372265689cc5b64ce0b4748a74a07ee3
Leonardo Lopes <leonardolopesborges@hotmail.com>
2023-08-20 23:57:31 -0300

    color add

--- script.js
+++ script.js
@@ -40,7 +40,8 @@ const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( {
      transparent: true,
      opacity: 0.5,
-     side: THREE.DoubleSide
+     side: THREE.DoubleSide,
+     color: 0x00ff00
 } );
 const cube = new THREE.Mesh( geometry, material );
 cube.position.y = geometry.parameters.height / 2;
