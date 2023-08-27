50c9f0b83a768849da420a233f7505fb60d686ad
Leonardo Lopes <leonardolopesborges@hotmail.com>
2023-08-27 16:46:53 -0300

    update

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 AFRAME.registerComponent('markerhandler', {
-    
     init: function () {
-        
-        this.toggle = false;
-        this.vid = document.querySelector("#marker1")
-        this.vide = document.querySelector("#marker2")
-       
+        this.mixedCube = document.querySelector('#mixedCube');
+        this.marker1 = document.querySelector('#marker1');
+        this.marker2 = document.querySelector('#marker2');
+
         alert("Marcadores se afastaram!");
-        
     },
     tick: function () {
-        const mixedCube = document.querySelector('#mixedCube');
-        const marker1 = document.querySelector('#marker1').object3D;
-        const marker2 = document.querySelector('#marker2').object3D;
-        
+        const marker1Visible = this.marker1.object3D.visible;
+        const marker2Visible = this.marker2.object3D.visible;
 
-        if (this.el.object3D.visible === true) {
-            const distance = marker1.position.distanceTo(marker2.position);
+        if (marker1Visible && marker2Visible) {
+            const marker1Position = new THREE.Vector3();
+            this.marker1.object3D.getWorldPosition(marker1Position);
 
-            if (!this.toggle && distance < 0.5) { // Ajuste a distância conforme necessário
-                this.toggle = true;
-                mixedCube.setAttribute('visible', 'true');
-                alert("Marcadores se aproximaram!");
-                
+            const marker2Position = new THREE.Vector3();
+            this.marker2.object3D.getWorldPosition(marker2Position);
+
+            const distance = marker1Position.distanceTo(marker2Position);
+
+            if (distance < 1.5) {
+                this.mixedCube.setAttribute('visible', 'true');
+                console.log("Marcadores próximos: Distância " + distance.toFixed(2));
+            } else {
+                this.mixedCube.setAttribute('visible', 'false');
+                console.log("Marcadores afastados: Distância " + distance.toFixed(2));
             }
+        } else {
+            this.mixedCube.setAttribute('visible', 'false');
+            alert("Aguardando a detecção de ambos os marcadores...");
         }
     }
 });
+
+
+
+
 
 
 
